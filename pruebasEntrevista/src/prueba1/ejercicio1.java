908698cc7ad6d538893bfc7d012111134a4299f6
kcrs <ferneyunimag@gmail.com>
2022-06-11 13:50:55 -0500

    begin

---
+++ pruebasEntrevista/src/prueba1/ejercicio1.java
@@ -0,0 +1,26 @@
+package prueba1;
+//Escribir unprograma utilizanso java que
+//encuentre lo dos elementos del arreglo que sumados dan 10
+//se deben imprimir ambos  numeros como resultado separados por un espacio
+//en el orden en que aparecene nel arreglo.
+public class ejercicio1 {
+
+    static int[] myArray = {1, 3, 4, 5, 6, 7, 2, 8, 9};
+
+    public static void main(String[] args) {
+        int count = 1;
+        for (int x : myArray) {
+            for (int i = count; i < myArray.length; i++) {
+                if (x + myArray[i] == 10) {
+                    System.out.println(x + ":" + myArray[i]);
+                }
+
+            }
+            count++;
+        }
+
+
+    }
+
+
+}
